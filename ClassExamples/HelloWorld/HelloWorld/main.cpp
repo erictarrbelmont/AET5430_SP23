947d257010c4d1181288b68bf04fdcb79db5e225
erictarrbelmont <erictarrbelmont@gmail.com>
2023-01-17 12:28:52 -0600

    Class work for 1/17

---
+++ ClassExamples/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Eric Tarr on 1/12/23.
+//
+
+#include <iostream>
+
+// function [out] = myFunction(input)
+
+// C++ data types:
+// numbers:
+// Integers: "int"
+// "float" : 32-bit decimal number
+// "double" : 64-bit decimal numbers
+
+// void function: doesn't return an output variable
+
+
+using namespace std;
+
+int main() {
+    
+    // Creating a variable:
+    int x; // "declare" a new int variable
+    x = 10; // "define" the variable
+    
+    float y = 4.5, z = -9.9;
+    
+    cout << "Hello, World!\n";
+    cout << x << endl;
+    cout << y << endl;
+    cout << z << endl;
+    return 0;
+}
