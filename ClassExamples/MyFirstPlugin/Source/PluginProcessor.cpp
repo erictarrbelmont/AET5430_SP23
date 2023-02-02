f14139a52056a117c85ea751af43e35bc17f8a0b
erictarrbelmont <erictarrbelmont@gmail.com>
2023-02-02 12:34:55 -0600

    Class 1/31

--- ClassExamples/MyFirstPlugin/Source/PluginProcessor.cpp
+++ ClassExamples/MyFirstPlugin/Source/PluginProcessor.cpp
@@ -133,26 +133,24 @@ void MyFirstPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
+        
+        // Have a loop to go through each of the samples in our signal
+        for (int n = 0 ; n < buffer.getNumSamples() ; ++n)
+        {
+            float x = buffer.getWritePointer(channel) [n];
+            
+            x = (2.f/M_PI) * atan(x * 25.f); // Distortion
+            
+            // scales amplitude by -12 dB
+            buffer.getWritePointer(channel) [n] = x;
+        }
+        
+        
     }
 }
 
