fb6426a039f71f7e822c10be7f0f7f71089af493
erictarrbelmont <erictarrbelmont@gmail.com>
2023-02-17 10:10:13 -0600

    Add amplitude knob

--- ClassExamples/MyFirstPlugin/Source/PluginEditor.cpp
+++ ClassExamples/MyFirstPlugin/Source/PluginEditor.cpp
@@ -15,7 +15,15 @@ MyFirstPluginAudioProcessorEditor::MyFirstPluginAudioProcessorEditor (MyFirstPlu
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (600, 400);
+    
+    gainKnob.addListener(this);
+    // Specify location in window (xPos,yPos,width,height)
+    gainKnob.setBounds(100,50,125,125);
+    gainKnob.setRange(-48.f,12.f,0.f); // (min, max, default)
+    gainKnob.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    gainKnob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 75, 25);
+    addAndMakeVisible(gainKnob);
 }
 
 MyFirstPluginAudioProcessorEditor::~MyFirstPluginAudioProcessorEditor()
@@ -28,13 +36,19 @@ void MyFirstPluginAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.setColour (juce::Colours::cornflowerblue);
+    g.setFont (35.0f);
+    g.drawFittedText ("My First Plugin", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void MyFirstPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+}
+
+
+void MyFirstPluginAudioProcessorEditor::sliderValueChanged(juce::Slider * slider)
+{
+    
 }
--- ClassExamples/MyFirstPlugin/Source/PluginEditor.h
+++ ClassExamples/MyFirstPlugin/Source/PluginEditor.h
@@ -14,7 +14,8 @@
 //==============================================================================
 /**
 */
-class MyFirstPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class MyFirstPluginAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                            public juce::Slider::Listener
 {
 public:
     MyFirstPluginAudioProcessorEditor (MyFirstPluginAudioProcessor&);
@@ -24,10 +25,14 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void sliderValueChanged(juce::Slider * slider) override;
+    
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     MyFirstPluginAudioProcessor& audioProcessor;
+    
+    juce::Slider gainKnob;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MyFirstPluginAudioProcessorEditor)
 };
